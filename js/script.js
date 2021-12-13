d0deaa1b3321f169068fda3323a339792ce2606f
Tyler Thietje <tylerthietje@gmail.com>
2021-12-13 13:25:04 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// targets div with class overview
+const overview = document.querySelector(".overview");
+const username = "tylerthietje";
+
+const getUser = async function () {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const data = await res.json();
+    console.log(data);
+    displayUserInfo(data);
+};
+getUser();
+
+const displayUserInfo = function (data){
+    const div =  document.createElement("div");
+    div.classList.add("user-info")
+    div.innerHTML =`
+    <figure>
+    <img alt="user avatar" src=${data.avatar_url} />
+  </figure>
+  <div>
+    <p><strong>Name:</strong> ${data.name}</p>
+    <p><strong>Bio:</strong> ${data.bio}</p>
+    <p><strong>Location:</strong> ${data.location}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+  </div> 
+  `;
+  overview.append(div);
+};
